e5d8b9800f9849092453a11997c92f850dcc0bad
aaronboogaard <aaron.boogaard@me.com>
2016-12-26 15:56:59 +0100

    Version 0.0.5
    - done with phone version as far as currently designed, except for
    modals

---
+++ js/functions.js
@@ -0,0 +1,36 @@
+// Get the modal
+// var modal = document.getElementById('myModal');
+// 
+// Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+// 
+// Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("closeButton closeModal")[0];
+// 
+// When the user clicks the button, open the modal 
+// btn.onclick = function() {
+// 	modal.style.display = "block";
+// }
+// 
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+// 	modal.style.display = "none";
+// }
+// 
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+// 	if (event.target == modal) {
+// 		modal.style.display = "none";
+// 	}
+// }
+
+function openModal(id) {
+	var modal = document.getElementById(id);
+	modal.style.display = "block";
+}
+function closeModal(id);{
+	document.getElementById(id).style.display = "none";
+}
+function myFunction(myMessage) {
+    alert(myMessage);
+}
